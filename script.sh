4f552da57fbf61a2a999ecb6531b31082b15b68c
Ing. Villena Daniel <ingenierovillena@gmail.com>
2018-05-20 16:47:04 -0300

    add - Main folder structure

--- script.sh
+++ script.sh
@@ -10,6 +10,17 @@ find ~/$projectName/ -type d |
 while read carpeta
 do
 
+   mkdir ${carpeta}/app/
+   mkdir ${carpeta}/app/dist/
+   mkdir ${carpeta}/app/docs/
+   mkdir ${carpeta}/app/logs/
+   mkdir ${carpeta}/app/resource/
+   mkdir ${carpeta}/app/src/
+   mkdir ${carpeta}/base/
+   mkdir ${carpeta}/test/
+   touch ${carpeta}/LICENSE
+   touch ${carpeta}/README.md
+
 done
 
 else
